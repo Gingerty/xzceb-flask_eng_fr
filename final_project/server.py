86c75cbd777a7aa346a34cfdc9402caff2ceae0a
Gingerty <101673472+Gingerty@users.noreply.github.com>
2022-08-07 18:04:01 -0500

    Update server.py

--- final_project/server.py
+++ final_project/server.py
@@ -1,24 +1,46 @@
 from machinetranslation import translator
 from flask import Flask, render_template, request
 import json
+import os
+from dotenv import load_dotenv
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
+
+load_dotenv()
+
+apikey = os.environ['apikey']
+url = os.environ['url']
+
+authenticator = IAMAuthenticator(apikey)
+language_translator = LanguageTranslatorV3(
+    version='2018-05-01',
+    authenticator=authenticator
+)
+
+language_translator.set_service_url(url)
 app = Flask("Web Translator")
 
 @app.route("/englishToFrench")
-def englishToFrench():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+def englishToFrench(english_text):
+    """Function trnalsates english top french
+    """
+    french = language_translator.translate(
+        text = english_text,
+        model_id= 'en-fr').get_result()
+    return french.get("translations")[0].get("translation")
 
 @app.route("/frenchToEnglish")
-def frenchToEnglish():
-    textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
-
+def frenchToEnglish(french_text):
+    """Function trnalsates english top french
+    """
+    english = language_translator.translate(
+        text = french_text,
+        model_id= 'fr-en').get_result()
+    return english.get("translations")[0].get("translation")
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
